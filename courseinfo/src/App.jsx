747667f185c1c7eb936341b1587ce494227e5a76
Milan Mazik <mazik1990@hotmail.com>
2024-05-10 00:30:53 +0200

    part1 1.3-1.5

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,31 +1,57 @@
 function App() {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   const Header = (props) => {
-    return <h1>{props.course}</h1>;
+    return <h1>{props.course.name}</h1>;
   };
-  const Content = (props) => {
+  const Content = () => {
     return (
       <>
-        <Part part={part1} exercise={exercises1} />
-        <Part part={part2} exercise={exercises2} />
-        <Part part={part3} exercise={exercises3} />
+        <Part
+          name={course.parts[0].name}
+          exercises={course.parts[0].exercises}
+        />
+        <Part
+          name={course.parts[1].name}
+          exercises={course.parts[1].exercises}
+        />
+        <Part
+          name={course.parts[2].name}
+          exercises={course.parts[2].exercises}
+        />
       </>
     );
   };
-  const Total = (props) => {
-    return <p>Number of exercises {props.prop1 + props.prop2 + props.prop3}</p>;
+  const Total = () => {
+    return (
+      <p>
+        Number of exercises{" "}
+        {course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises}
+      </p>
+    );
   };
   const Part = (props) => {
     return (
       <p>
-        {props.part} {props.exercise}{" "}
+        {props.name} {props.exercises}
       </p>
     );
   };
@@ -33,8 +59,8 @@ function App() {
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total prop1={exercises1} prop2={exercises2} prop3={exercises3} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 }
